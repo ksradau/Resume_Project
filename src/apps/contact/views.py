cf5e7461d99334aa40161e9acbcec0f4e96ca5ae
ksradau <sushei.ekaterina@gmail.com>
2020-04-11 16:37:28 +0300

    Tests added

--- src/apps/contact/views.py
+++ src/apps/contact/views.py
@@ -1,6 +1,3 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import TemplateView
 
 
--- src/apps/index/tests.py
+++ src/apps/index/tests.py
@@ -1,3 +1,20 @@
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+from apps.index.views import IndexView
+
+
+class Test(TestCase):
+    def setUp(self) -> None:
+        self.cli = Client()
+
+    def test_get(self):
+        resp = self.cli.get("/")
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.templates), 2)
+        self.assertEqual(
+            [_t.name for _t in resp.templates], ["index/index.html", "base.html"]
+        )
+        self.assertEqual(
+            resp.resolver_match.func.__name__, IndexView.as_view().__name__
+        )

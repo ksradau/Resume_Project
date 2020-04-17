140ae1b494f5c647a409ae804f41ce87b7a41e39
Your Name <you@example.com>
2020-04-17 21:42:14 +0300

    More settings in Mirgation

--- src/apps/index/models.py
+++ src/apps/index/models.py
@@ -5,3 +5,9 @@ class UserInfo(m.Model):
     name = m.TextField(unique=True)
     greeting = m.TextField(null=True, blank=True)
     age = m.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "User Info"
+
+    def __str__(self):
+        return f"UserInfo(id={self.pk}, name={self.name!r})"

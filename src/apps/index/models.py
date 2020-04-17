ffa9ff0681bd63918dfabeb09ad3f5d6391b9f56
Your Name <you@example.com>
2020-04-17 20:48:55 +0300

    Index model added

--- src/apps/index/models.py
+++ src/apps/index/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class UserInfo(m.Model):
+    name = m.TextField(unique=True)
+    greeting = m.TextField(null=True, blank=True)
+    age = m.IntegerField(null=True, blank=True)

c607b1ac7927268225607467763d517a7fa4133a
Your Name <you@example.com>
2020-04-17 22:00:29 +0300

    New

--- src/apps/index/views.py
+++ src/apps/index/views.py
@@ -1,5 +1,16 @@
 from django.views.generic import TemplateView
+from apps.index.models import UserInfo
 
 
 class IndexView(TemplateView):
     template_name = "index/index.html"
+
+    def get_context_data(self, **kwargs):
+        parent_ctx = super().get_context_data(**kwargs)
+
+        info = UserInfo.objects.first()
+        ctx = {"name": info.name, "greeting": info.greeting}
+
+        ctx.update(parent_ctx)
+
+        return ctx

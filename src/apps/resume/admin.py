126023aa6646dbe2d8d056b318161426e969268f
ksradau <sushei.ekaterina@gmail.com>
2020-04-23 00:40:29 +0300

    Resume Page changing, new fields in model Project

--- src/apps/resume/admin.py
+++ src/apps/resume/admin.py
@@ -6,22 +6,30 @@ from apps.resume.models import Technology
 from apps.resume.models import Project
 from apps.resume.models import Responsibility
 
+from project.utils.forms import gen_textinput_admin_form
+
 
 @admin.register(ResumePage)
 class ResumePageAdminModel(ModelAdmin):
-    pass
+    form = gen_textinput_admin_form(
+        ResumePage, (ResumePage.title, ResumePage.h1, ResumePage.preview)
+    )
 
 
 @admin.register(Technology)
 class TechnologyAdminModel(ModelAdmin):
-    pass
+    form = gen_textinput_admin_form(
+        Technology, (Technology.name, Technology.description)
+    )
 
 
 @admin.register(Project)
-class TechnologyAdminModel(ModelAdmin):
-    pass
+class ProjectAdminModel(ModelAdmin):
+    form = gen_textinput_admin_form(
+        Project, (Project.link, Project.name)
+    )
 
 
 @admin.register(Responsibility)
-class TechnologyAdminModel(ModelAdmin):
+class ResponsibilityAdminModel(ModelAdmin):
     pass
--- src/apps/resume/models.py
+++ src/apps/resume/models.py
@@ -13,22 +13,42 @@ class ResumePage(m.Model):
         verbose_name_plural = "Resume Page"
 
     def __str__(self):
-        return f"ResumePage(id={self.pk})"
+        return f"ResumePage [id_{self.pk}]"
 
 
 class Technology(m.Model):
     name = m.TextField()
     description = m.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Technologies"
+
+    def __str__(self):
+        return f"{self.name} [id_{self.pk}]"
+
 
 class Project(m.Model):
     started_at = m.DateField(null=True, blank=True)
     finished_at = m.DateField(null=True, blank=True)
     summary = m.TextField(null=True, blank=True)
+    achievements_text = m.TextField(null=True, blank=True)
     technologies = m.ManyToManyField(Technology, related_name="project")
+    link = m.TextField(null=True, blank=True)
+    name = m.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return f"{self.name} [id_{self.pk}]"
 
 
 class Responsibility(m.Model):
     summary = m.TextField()
     project = m.ForeignKey(Project, on_delete=m.CASCADE, related_name="responsibilities")
 
+    class Meta:
+        verbose_name_plural = "Responsibilities"
+
+    def __str__(self):
+        return f"Responsibility [id_{self.pk}]"
